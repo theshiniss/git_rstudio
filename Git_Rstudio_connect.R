5d457cdb1323977f2304551bc0697170c911e9b0
TheShinISS <marco.massari@iss.it>
2025-02-16 00:11:02 +0100

    First Commit

---
+++ Git_Rstudio_connect.R
@@ -0,0 +1,7 @@
+# collegare Rstudio a Github
+
+install.packages("usethis")
+library(usethis)
+use_git_config(user.name = "TheShinISS", user.email = "marco.massari@iss.it")
+create_github_token()
+gitcreds::gitcreds_set()
