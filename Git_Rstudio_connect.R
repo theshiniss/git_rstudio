b9b6a946046a77e9394836aff6e6def1bf8674db
TheShinISS <marco.massari@iss.it>
2025-02-16 00:27:00 +0100

    Inserito link

--- Git_Rstudio_connect.R
+++ Git_Rstudio_connect.R
@@ -5,3 +5,7 @@ library(usethis)
 use_git_config(user.name = "TheShinISS", user.email = "marco.massari@iss.it")
 create_github_token()
 gitcreds::gitcreds_set()
+
+
+# Per collegare un progetto Rstudio già esistente a un nuovo appena creato repository Github:
+# https://www.carlosivanrodriguez.com/guides/workflow/connect-an-existing-rstudio-project-to-github/
