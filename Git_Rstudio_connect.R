5cabdc37c13d16e0edc145ae23ce9582baa8e2cf
TheShinISS <marco.massari@iss.it>
2025-02-17 16:48:02 +0100

    Aggiunto check iniziali per verificare se git è installato

--- Git_Rstudio_connect.R
+++ Git_Rstudio_connect.R
@@ -1,8 +1,15 @@
+# verificare se git e' installato
+# su Terminal: git --version
+
+# verificare il file di configurazione di git
+# su Terminal: git config --list 
+
+
 # collegare Rstudio a Github
 
 install.packages("usethis")
 library(usethis)
-use_git_config(user.name = "TheShinISS", user.email = "marco.massari@iss.it")
+use_git_config(user.name = "TheShinISS", user.email = "marco.massari@iss.it", scope = c("project"))
 create_github_token()
 gitcreds::gitcreds_set()
 
