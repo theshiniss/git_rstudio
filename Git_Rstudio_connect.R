aacd2fc342cb5b8a9187694f06d260c56d6ce3fe
TheShinISS <marco.massari@iss.it>
2025-02-19 15:03:38 +0100

    Aggiunto il timeout

--- Git_Rstudio_connect.R
+++ Git_Rstudio_connect.R
@@ -1,6 +1,5 @@
 ## guarda questo: https://gist.github.com/Z3tt/3dab3535007acf108391649766409421
 ## su linux non funziona sempre la memorizzazione permanente delle credenziali con l'opzione https
-## quindi si può usare SSH, guarda qui: https://www.youtube.com/watch?v=_Bn78aor_N4
 
 # verificare se git e' installato
 # su Terminal: git --version
@@ -17,9 +16,15 @@
 
 install.packages("usethis")
 library(usethis)
-use_git_config(user.name = "TheShinISS", user.email = "marco.massari@iss.it", scope = c("project"))
+use_git_config(user.name = "TheShinISS", user.email = "marco.massari@iss.it")
 create_github_token()
 gitcreds::gitcreds_set()
+
+## Note for Linux users:
+## credentials::set_github_pat() (in line 34) might store your PAT in a memory cache that
+## expires after 15 minutes or when the computer is rebooted. You thus may wish to do 
+## extend the cache timeout to match the PAT validity period:
+usethis::use_git_config(credential.helper="cache --timeout=2600000") #> cache timeout ~30 days
 
 
 # Per collegare un progetto Rstudio già esistente a un nuovo appena creato repository Github:
